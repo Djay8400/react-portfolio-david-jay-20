b23a5c78fa029e125759f563dae110ea7b90bf2c
David Jay <djay8400@gmail.com>
2021-12-14 17:29:25 -0600

    successfully added and rendered About comp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
+import About from "./components/About";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <Header />;
+      <About />;
       <Footer />;
     </div>
   );
