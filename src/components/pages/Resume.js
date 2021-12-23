d506b79ca2fbd9585740299513e5f6f4d526b1c6
David Jay <djay8400@gmail.com>
2021-12-22 20:47:26 -0600

    added resume page link to resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,2 +1,13 @@
-// - Resume Section
-// 	-Downloadable resume link and list of skills/badges
+import React from "react";
+
+function Resume() {
+  return (
+    <div>
+      <a href="./assets/Resume-David-Jay.pdf" className="gitLinked">
+        My Resume
+      </a>
+    </div>
+  );
+}
+
+export default Resume;
